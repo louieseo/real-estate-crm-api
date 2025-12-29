850d2fa1778c84bf2271e8ee680b6a39ea4d6c67
Louie Seo <louieseo@gmail.com>
2025-12-29 18:49:25 +0900

    20251229

--- src/middlewares/isAdmin.js
+++ src/middlewares/isAdmin.js
@@ -1,5 +1,6 @@
 module.exports =function (req, res, next) {
-// auth 미들웨어가 먼저 실행되어야 함
+// auth 미들웨어가 먼저 실행되지만
+// 아래에 req.user 을 작성해둔 이유는 “auth 없이도 isAdmin을 쓰는 경우”를 대비한 것
 if (!req.user) {
 return res.status(401).json({
 message:"Unauthorized",
